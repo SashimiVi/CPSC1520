3beee5ec9403f3ca5d463ded34820bb54afd4c34
SashimiVi <wding1@ualberta.ca>
2023-09-27 07:31:13 -0600

    fixed

--- js/index.js
+++ js/index.js
@@ -1,3 +1,4 @@
+
 const getQuoteButton = document.querySelector('#getQuote');
 const getFirstName = document.querySelector('#input_first_name');
 const getLastName = document.querySelector('#input_last_name');
@@ -6,10 +7,13 @@ const updateCardDisplay = document.querySelector('output');
 getQuoteButton.addEventListener('click', onGetQuote);
 
 function onGetQuote(e) {
+    // Prevent the default form submission
     e.preventDefault()
     const firstName = getFirstName.value;
     const lastName = getLastName.value;
+     // Combine the first name and last name to create the full nam
     const fullName =  `${firstName} ${lastName}`
+    // Update the content of the element to display the full name
     updateCardDisplay.textContent = fullName
     
 }
