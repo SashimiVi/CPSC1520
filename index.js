247c7598076a89e09af0fa88b07fa89773e49dec
SashimiVi <wding1@ualberta.ca>
2023-09-27 06:51:43 -0600

    init

---
+++ index.js
@@ -0,0 +1,16 @@
+const getQuoteButton = document.querySelector('#getQuote');
+const getFirstName = document.querySelector('#input_first_name');
+const getLastName = document.querySelector('#input_last_name');
+const updateCardDisplay = document.querySelector('output');
+
+getQuoteButton.addEventListener('click', onGetQuote);
+
+function onGetQuote(e) {
+    e.preventDefault()
+    const firstName = getFirstName.value
+    const lastName = getLastName.value
+    const fullName =  firstName + ' ' + lastName
+    updateCardDisplay.textContent = fullName
+    
+}
+
